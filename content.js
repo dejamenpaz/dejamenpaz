a2a86f9e32c10956c6ddccc8fe384d3cedc5fb9f
dejamenpaz <>
2019-09-18 07:45:11 -0400

    Habilito escritura de passwords

--- content.js
+++ content.js
@@ -1,4 +1,17 @@
-var images = document.getElementsByTagName('img');
-for (var i = 0, l = images.length; i < l; i++) {
-  images[i].src = 'http://placekitten.com/' + images[i].width + '/' + images[i].height;
+console.log('Dejamenpaz iniciado');
+// el de Itau
+var x = document.getElementById('InputValuePIN');
+if (x) {
+  x.removeAttribute('readonly');
+  console.log('Liberado de Itau');
+}
+
+//Todo el resto(Vision, Familiar)
+var inputs = document.getElementsByTagName('input');
+for (var i = 0; i < inputs.length; i++) {
+  x = inputs[i];
+  if (x.type.toLowerCase() == 'password') {
+    x.removeAttribute('readonly');
+    console.log('Password habilitado');
+  }
 }
